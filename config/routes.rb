c6a28b05c1cded7c5d4da8c8114e785346583a2a
Ivan Dovhai <troopermanowar@gmail.com>
2021-04-20 16:39:45 +0300

    First routes - main page and admin part

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'main#index'
+  get '/articles', to: 'main#index'
+
+  namespace 'admin' do
+    root 'main#index'
+  end
 end
