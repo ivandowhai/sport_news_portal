2df884b235a1a244dc21c759d743c3b8fe010196
Ivan Dovhai <troopermanowar@gmail.com>
2021-04-30 10:40:47 +0300

    Standardrb

--- spec/controllers/main_controller_spec.rb
+++ spec/controllers/main_controller_spec.rb
@@ -2,6 +2,6 @@ require "rails_helper"
 
 RSpec.describe MainController, type: :controller do
   describe "GET /" do
-    it { should route(:get, '/').to(action: :index) }
+    it { should route(:get, "/").to(action: :index) }
   end
 end
--- spec/controllers/page_controller_spec.rb
+++ spec/controllers/page_controller_spec.rb
@@ -2,6 +2,6 @@ require "rails_helper"
 
 RSpec.describe PagesController, type: :controller do
   describe "GET pages#show" do
-    it { should route(:get, '/pages/about-sports-hub').to(action: :show, slug: 'about-sports-hub') }
+    it { should route(:get, "/pages/about-sports-hub").to(action: :show, slug: "about-sports-hub") }
   end
 end
