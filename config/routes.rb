023988f4fb2136b9c513cb689ac193d10ccb337b
Ivan Dovhai <troopermanowar@gmail.com>
2021-05-13 14:08:46 +0300

    Refactor and improvements

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@
 Rails.application.routes.draw do
   root 'main#index'
 
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'auth/sessions' }
 
-  scope module: 'auth' do
-    devise_scope :user do
-      get '/sign_out', to: 'sessions#destroy'
-      get '/sign_up', to: 'registrations#new'
-      get '/sign_in', to: 'sessions#new'
-    end
-  end
+  # scope module: 'auth' do
+  #   devise_scope :user do
+  #     get '/sign_out', to: 'sessions#destroy'
+  #     get '/sign_up', to: 'registrations#new'
+  #     get '/sign_in', to: 'sessions#new'
+  #   end
+  # end
 
   resources :categories do
     resources :articles do
