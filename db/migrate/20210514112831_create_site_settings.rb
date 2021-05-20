8931c1c0a99183094ea0edc4788cf8ec21fcef37
Ivan Dovhai <troopermanowar@gmail.com>
2021-05-20 13:44:28 +0300

    fix migrations

--- db/migrate/20210514112831_create_site_settings.rb
+++ db/migrate/20210514112831_create_site_settings.rb
@@ -2,7 +2,7 @@ class CreateSiteSettings < ActiveRecord::Migration[6.1]
   def change
     create_table :site_settings do |t|
       t.string :key
-      t.json :settings
+      t.json :parameters
     end
   end
 end
