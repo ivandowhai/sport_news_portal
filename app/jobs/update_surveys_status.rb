baea6bf07c168f9a8de0ead2e4c07290966ae1ed
Ivan Dovhai <troopermanowar@gmail.com>
2021-05-26 17:15:37 +0300

    Surveys - update status

---
+++ app/jobs/update_surveys_status.rb
@@ -0,0 +1,10 @@
+require "sidekiq-scheduler"
+
+class UpdateSurveysStatus
+  include Sidekiq::Worker
+
+  def perform
+    Survey.where('start <= ?', Time.now).update(status: Survey::STATUS_PUBLISHED)
+    Survey.where('"end" <= ?', Time.now).update(status: Survey::STATUS_CLOSED)
+  end
+end
