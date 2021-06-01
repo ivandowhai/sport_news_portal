37a65b0d9e0a6e3febb97256a81724fa1a10f311
Ivan Dovhai <troopermanowar@gmail.com>
2021-06-01 10:35:39 +0300

    Fix banners, remove turbolink

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,6 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
@@ -22,5 +21,4 @@ import "../controllers/article"
 import "../controllers/surveys"
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
