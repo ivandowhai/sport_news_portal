66277afe6cfa0c2112994873d100a0ae7c81911e
Ivan Dovhai <troopermanowar@gmail.com>
2021-06-02 13:00:23 +0300

    Teams - Fix tests

--- spec/factories/team.rb
+++ spec/factories/team.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :team do
     category { association :category }
     subcategory { association :category }
-    team { Faker::Team::name }
-    location { Faker::Team::state }
-    logo { Rack::Test::UploadedFile.new('spec/image_for_tests.jpg', 'image/png') }
+    team { Faker::Team.name }
+    location { Faker::Team.state }
+    logo { Rack::Test::UploadedFile.new("spec/image_for_tests.jpg", "image/png") }
   end
 end
